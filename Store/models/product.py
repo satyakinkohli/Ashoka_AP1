205bf1290a7c9bd1b60e93296e3be2af07637587
Satyakin Kohli <satyakinkohli@Satyakins-MacBook-Air.local>
2020-10-10 17:38:10 +0530

    Added Store app

---
+++ Store/models/product.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    availability = models.BooleanField()
+    description = models.CharField(max_length=250, default="Please buy this. The seller is broke and needs the money")
+    image = models.ImageField(upload_to='products/')
