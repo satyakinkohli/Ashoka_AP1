1e76f8650afed754936994d1a74293d72b4b88e3
Satyakin Kohli <satyakinkohli@Satyakins-MacBook-Air.local>
2020-10-10 19:22:47 +0530

    Created super users + category

--- Store/models/product.py
+++ Store/models/product.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     availability = models.BooleanField()
     description = models.CharField(max_length=250, default="Please buy this. The seller is broke and needs the money")
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='upload/products/')
