971651563cd9d6fede426ccd5b078af5f0581c0c
adititibarewal26 <aditi.tibarewal_ug22@ashoka.edu.in>
2020-10-12 04:05:43 +0530

    Nav bar and square products

--- Store/views.py
+++ Store/views.py
@@ -6,10 +6,16 @@ from django.http import HttpResponse
 
 from .models.product import Product
 
+from .models.category import Category
+
 
 def index(request):
     products = Product.get_all_product()
+    categories = Category.get_all_categories()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
 
-    return render(request, 'index.html', {'products': products})
+    return render(request, 'index.html', data)
     # return HttpResponse("Request Received")
 
