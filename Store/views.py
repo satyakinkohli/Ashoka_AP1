02dc7bdae2c0d9c6631ad9f298bb93f245793879
Satyakin Kohli <satyakinkohli@Satyakins-MacBook-Air.local>
2020-10-21 06:41:15 +0530

    Fail

--- Store/views.py
+++ Store/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect , HttpResponseRedirect
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import RequestContext
 
 from .models.product import Product
 from .models.category import Category
